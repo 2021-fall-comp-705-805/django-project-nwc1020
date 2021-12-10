e48fcc586261182051578a7d37c08ab2c7145998
nwc1020 <nwc1020@wildcats.unh.edu>
2021-12-10 13:39:42 -0500

    changing the router

--- config/api_router.py
+++ config/api_router.py
@@ -2,10 +2,10 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 from flight.Airports.views import RRViewSet
 
-if settings.DEBUG:
-    router = DefaultRouter()
-else:
-    router = SimpleRouter()
+#if settings.DEBUG:
+router = DefaultRouter()
+#else:
+#    router = SimpleRouter()
 
 router.register("RecentRequests", RRViewSet)
 
