40128dfd921b7f49384518b887b0ceba32b526d6
nwc1020 <nwc1020@wildcats.unh.edu>
2021-12-09 21:35:17 -0500

    went through a couple of iterations of how i wanted to get the big list of airports to use, i tried to keep it inside of the models file however it wouldnt serialize so my workaround was to move it to its own file which should only be run if i need an update anyways as id like to keep the list the same for the most part and then read from the file turning it from dict that i stored it in into the list of tuples that works with my model

--- flight/users/models.py
+++ flight/users/models.py
@@ -2,24 +2,14 @@ from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-#from django.db import models
+import json
+
 
 
 
 
 class User(AbstractUser):
     """Default user for Flight."""
-    Airport = [
-    ('ATL', 'Hartsfield-Jackson International Airport'),
-    ('LAX', 'Los Angeles International Airport'),
-    ('JFK', 'John F. Kennedy International Airport'),
-    ('BOS', 'Logan International Airport'),
-    ('MCO', 'Orlando International Airport'),
-    ]
-    defaultAirport = CharField(max_length=3, choices=Airport, default='BOS')
-    first_name = None  # type: ignore
-    last_name = None  # type: ignores
-
     def get_absolute_url(self):
         """Get url for user's detail view.
 
@@ -28,11 +18,13 @@ class User(AbstractUser):
 
         """
         return reverse("users:detail", kwargs={"username": self.username})
+    
+    with open('airportcodes.txt','r') as f:
+        lines = f.read()
+    test = json.loads(lines)
+    Airport = list = list(test.items())
+    defaultAirport = CharField(max_length=3, choices=Airport, default='BOS')
+    first_name = None  # type: ignore
+    last_name = None  # type: ignores
 
-
-# class Amadeus(models.Model):
-#     Origin = models.CharField(max_length=3)
-#     Dest = models.CharField(max_length=3)
-#     Startdate = models.DateField()
-#     Returndate = models.DateField()
 
