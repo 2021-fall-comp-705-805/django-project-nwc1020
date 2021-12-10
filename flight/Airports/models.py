b3d52729bbf00c56543f1b3d374142291863d74e
nwc1020 <nwc1020@wildcats.unh.edu>
2021-12-10 09:33:38 -0500

    redoing all of my migrations to see if that helps

--- flight/Airports/models.py
+++ flight/Airports/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Airports(models.Model):
-    IATAcode = models.CharField(max_length=3,default='BOS')
+    IATAcode = models.CharField(max_length=3,default='BOS',primary_key=True)
     Airportname = models.CharField(max_length=255,default='Gen. Edward Lawrence Logan International Airport')
 
 class RecentRequest(models.Model):
